91f600ce8060cb4418f3f90e45c45ecc8f8be8cb
Larissa Diniz <larissa.dtm@gmail.com>
2020-08-19 09:51:29 -0300

    include empty project and podfile

---
+++ cuteDogs/cuteDogs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cuteDogs
+//
+//  Created by Larissa Diniz on 19/08/20.
+//  Copyright © 2020 Larissa Diniz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
